8bf1e4470094e216701ac68e69cd81ff9f2a62d1
astral <astral@astralchan.xyz>
2024-08-22 19:20:12 -0600

    Add check for pwd

--- test/ls/check.sh
+++ test/ls/check.sh
@@ -3,4 +3,16 @@
 # Copyright (C) astral
 # See COPYING for details.
 
+current_dir=$(pwd)
+test_dir=$(dirname $0)
+
+check_value() {
+	if [ "$1" = "$2" ]; then
+		echo "check passed"
+	else
+		echo "check failed"
+		exit 1
+	fi
+}
+
 exit 0
--- test/pwd/check.sh
+++ test/pwd/check.sh
@@ -3,4 +3,21 @@
 # Copyright (C) astral
 # See COPYING for details.
 
+current_dir=$(pwd)
+test_dir=$(dirname $0)
+
+pwd=$current_dir/src/pwd
+
+check_value() {
+	printf "expected: %s\ngot: %s\n" "$1" "$2"
+	if [ "$1" = "$2" ]; then
+		echo "check passed"
+	else
+		echo "check failed"
+		exit 1
+	fi
+}
+
+check_value "$current_dir" "$($pwd)"
+
 exit 0
